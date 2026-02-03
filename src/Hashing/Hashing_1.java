76eca6fc8b3c36e4bd1405ab430290f16d143f88
Aneeshsshankar <aneeshsshankar@gmail.com>
2026-02-03 19:57:56 +0530

    Started Hashing

---
+++ src/Hashing/Hashing_1.java
@@ -0,0 +1,26 @@
+package Hashing;
+
+import java.util.Scanner;
+
+public class Hashing_1 {
+    public static void main(String[] args) {
+
+        //Original Array
+        int[] arr = {1,2,1,3,2};
+
+        //Hash Array
+        int[] hash = new int[13];
+        for(int i=0 ; i< arr.length;i++){
+            hash[arr[i]] += 1 ;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+
+        while(q>0){
+            int num = sc.nextInt();
+            System.out.println(hash[num]);
+            q--;
+        }
+    }
+}
